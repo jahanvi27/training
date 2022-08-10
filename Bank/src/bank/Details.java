b4f242dc37020aeab25db51aa9605a4c11530df2
jahanvi27 <jahanvigoyal3344@gmail.com>
2022-08-10 12:24:32 +0530

    Committing details

---
+++ Bank/src/bank/Details.java
@@ -0,0 +1,19 @@
+package bank;
+
+public class Details {
+	
+	String dateReceived;
+	String product;
+	String subProduct;
+	String issue;
+	String subIssue;
+	String company;
+	String state;
+	String ZIPcode;
+	String submittedVia;
+	String dateSentToCompany;
+	String companyResponseToConsumer;
+	String timelyResponse;
+	String consumerDisputed;
+
+}
